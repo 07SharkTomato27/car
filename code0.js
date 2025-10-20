f35ccb8e44d94b479359891830620e1f68737fe2
_SharkTomato_ <142010343+07SharkTomato27@users.noreply.github.com>
2025-10-20 16:21:09 -0700

    a

---
+++ code0.js
@@ -0,0 +1,21 @@
+gdjs.Game_32SceneCode = {};
+gdjs.Game_32SceneCode.localVariables = [];
+gdjs.Game_32SceneCode.idToCallbackMap = new Map();
+
+
+gdjs.Game_32SceneCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Game_32SceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Game_32SceneCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['Game_32SceneCode'] = gdjs.Game_32SceneCode;
